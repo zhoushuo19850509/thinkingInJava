4066e8e84e0cb1d415f684f6353ac31f19e09aa3
zhoushuo <zhoushuo19850509@163.com>
2019-03-16 22:50:40 +0800

    add Alien.java FreezeAlien.java and ThawAlien.java

---
+++ ThinkingInJava/src/com/nbcb/thinkingInJava/io/ThawAlient.java
@@ -0,0 +1,25 @@
+package com.nbcb.thinkingInJava.io;
+
+import java.io.*;
+
+public class ThawAlient {
+    public static void main(String[] args){
+        ObjectInputStream in = null;
+        try {
+            in = new ObjectInputStream(new FileInputStream(new File("alien.out")));
+            Object object = in.readObject();
+            System.out.println(object.getClass());
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } finally{
+            try {
+                in.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+}
