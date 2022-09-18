65ae5ab09bf9b2358ba2471d066fa5d7e52b1b1b
zhoushuo19850509 <zhoushuo19850509@163.com>
2022-09-18 15:27:54 +0800

    I/O 相关代码初始化

--- ThinkingInJava/src/com/nbcb/thinkingInJava/arrays/AlphabeticSearch.java
+++ ThinkingInJava/src/com/nbcb/thinkingInJava/arrays/AlphabeticSearch.java
@@ -27,7 +27,5 @@ public class AlphabeticSearch {
                 System.out.println(str);
             }
         }
-
-
     }
 }
--- ThinkingInJava/src/com/nbcb/thinkingInJava/io/DirList.java
+++ ThinkingInJava/src/com/nbcb/thinkingInJava/io/DirList.java
@@ -8,18 +8,18 @@ import java.util.regex.Pattern;
 public class DirList {
 	
 	public static void main(String[] args){
-		File path = new File("src\\com\\nbcb\\thinkingInJava\\userInterface");  // 工程当前目录
+		File path = new File("src" +File.separator+
+				"com"+ File.separator+"nbcb" + File.separator +
+				"thinkingInJava" + File.separator +
+				"userInterface");  // 工程当前目录
 		
 		String[] list ; // 保存文件名列表
 		
 		System.out.println(path.getAbsolutePath());
-		
-		if(args.length == 0){
-			list = path.list();
-		}else{
-			list = path.list(new DirFilter(args[0]));   // filter with regular expression:  D.*\.java
-		}		
-		
+		String reg = "*.java";
+
+		list = path.list(new DirFilter(reg));
+
 		Arrays.sort(list,String.CASE_INSENSITIVE_ORDER); // sort the array
 		
 		for(String fileName : list){
